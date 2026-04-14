891921a91b1762cb94eb862442c337ffdc447c63
mr-ekansh <67186634+mr-ekansh@users.noreply.github.com>
2026-04-14 12:00:06 +0530

    B2B error changes

--- Assets/script/Base/SocketController.cs
+++ Assets/script/Base/SocketController.cs
@@ -309,10 +309,21 @@ public class SocketController : MonoBehaviour
 
     private void OnError(Error err)
     {
-        Debug.LogError("Socket Error Message: " + err);
-#if UNITY_WEBGL && !UNITY_EDITOR
-    JSManager.SendCustomMessage("error");
-#endif
+        Debug.LogError("[ERROR] Socket error: " + err);
+        if (!string.IsNullOrEmpty(err.message) && err.message.Contains("Session expired"))
+        {
+        Debug.LogWarning("Session expired detected");
+        OnDisconnected();
+    #if UNITY_WEBGL && !UNITY_EDITOR
+        JSManager.SendCustomMessage("session_expired");
+    #endif
+        }
+        else
+        {
+    #if UNITY_WEBGL && !UNITY_EDITOR
+        JSManager.SendCustomMessage("error");
+    #endif
+        }
     }
 
     private void OnListenEvent(string data)
@@ -577,7 +588,7 @@ public class FreeSpins
     public bool isFreeSpin { get; set; }
 }
 
-[SerializeField]
+[Serializable]
 public class Bonus
 {
     public int BonusSpinStopIndex { get; set; }
